6af872b377772818b6871cb8f40c39445b693f05
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2023-10-30 20:52:24 +0530

    Create SplittingTheData.py

---
+++ SplittingTheData.py
@@ -0,0 +1,7 @@
+X = data.iloc[:,:-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test =train_test_split(
+X, y, test_size = 0.2, random_state = 24)
+
+print(f"Train: {X_train.shape}, {y_train.shape}")
+print(f"Test: {X_test.shape}, {y_test.shape}")
